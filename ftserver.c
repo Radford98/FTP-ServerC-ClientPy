e865cb7b9fda27d98de850e430c86d04b54959d2
Radford98 <powelbra@oregonstate.edu>
2019-11-14 21:49:19 -0800

    Server: Startup

---
+++ ftserver.c
@@ -0,0 +1,80 @@
+/* Author: Brad Powell
+ * Program: ftserver.c
+ * Description: An ftp server for transferring text files with ftclient. It waits on the port specified in
+ * the command line for a request from ftclient and will either display the current directory or send
+ * the requested file.
+ * Course: CS 372-400 Computer Networks
+ * Last Modified: 11/14/2019
+ * Citations:
+ * 	Basic framework (setting up socket, how the send and recv commands work, etc.) based on material from cs344
+ * 	Beej's guide (http://beej.us/guide/bgnet/) was used as a reference.
+ * 	Info on how to use fread to read the contents of an entire file:
+ * 		https://stackoverflow.com/questions/14002954/c-programming-how-to-read-the-whole-file-contents-into-a-buffer
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+/* Error(const char* msg): Combines perror with exit(0) for error reporting. Saves on typing exit(0) multiple times.
+ * Pre: Error message.
+ * Post: Program closed.
+ */
+void Error(const char *msg) {
+	perror(msg);
+	exit(0);
+}
+
+/* startup(int port): Creates and sets up a socket on the port number. Begins listening, but only accepts 1
+ * connection at a time.
+ * Pre: Port in int form.
+ * Post: Created and listening socket. Returns listening socket file descriptor.
+ */
+int startup(int port) {
+	int listenSocket;
+	struct sockaddr_in serverAddress;
+
+	// Set up the server address struct
+	memset((char *)&serverAddress, '\0', sizeof(serverAddress));	// Clear address struct
+	serverAddress.sin_family = AF_INET;				// Create socket for network
+	serverAddress.sin_port = htons(port);				// Convert port number
+	serverAddress.sin_addr.s_addr = INADDR_ANY;			// Allow any address
+
+	// Set up socket
+	listSocket = socket(AF_INET, SOCK_STREAM, 0);			// Create TCP socket
+	if (listenSocket < 0) {
+		Error("ERROR creating socket.");
+	}
+
+	// Bind socket with port and begin listening
+	if (bind(listenSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
+		Error("ERROR binding socket.");
+	}
+	listen(listenSocket, 1);					// Need to accept 1 connection at a time
+
+	return listenSocket;
+
+}
+
+
+
+int main(int argc, char *argv[]) {
+
+
+	if (argc != 2) {
+		fprintf(stderr, "USAGE: ./%s server_port\n", argv[0]);
+		exit(0);
+	}
+
+	// atoi(argv[1]) for portnum in int
+
+
+	return 0;
+}
